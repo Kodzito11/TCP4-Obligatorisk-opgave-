09f9c364d161fc1ad8b5c092b54c4713c31cb796
Kodzito11 <Kos001@edu.zealand.dk>
2024-10-23 10:29:10 +0200

    Add project files.

---
+++ TCP4(Obligatorisk opgave)/ServerTCP.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+
+
+TcpListener listener = new TcpListener(IPAddress.Any, 7);
+listener.Start();
+
+while (true) ;
+{
+	TcpClient socket = listener.AcceptTcpClient();
+	IPEndPoint externalEndPoint = socket.Client.RemoteEndPoint as IPEndPoint;
+
+	Task.Run(() => HandleClient(socket));
+}
+
+void HandleClient(TcpClient socket)
+{
+	//[Klassen] [Angivet instans af NS] = [Angivet instanst af socket] [Metode til at give/få strøm af data]
+	NetworkStream networkStream = socket.GetStream();
+
+	StreamReader reader = new StreamReader(networkStream);
+	StreamWriter writer = new StreamWriter(networkStream);
+
+	try
+	{
+		while (socket.Connected)
+		{
+
+			string? commandresponse = reader.ReadLine();
+
+			if (commandresponse == null) break;
+
+			writer.WriteLine("Insert numbers");
+			writer.Flush();
+
+			string? numberInput = reader.ReadLine();
+			if (numberInput == null) break;
+
+			string[] numbers
+
+		}
+	}
+
+}
+
